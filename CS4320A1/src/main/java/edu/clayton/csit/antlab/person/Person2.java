ef07989fe0b1e913a3085e567a98f1bc4abdc24f
david92569 <89665621+david92569@users.noreply.github.com>
2021-08-27 18:54:23 -0400

    Update Person2.java

--- CS4320A1/src/main/java/edu/clayton/csit/antlab/person/Person2.java
+++ CS4320A1/src/main/java/edu/clayton/csit/antlab/person/Person2.java
@@ -1,3 +1,13 @@
+package edu.clayton.csit.antlab.person;
+
+/**
+ *  A simple class for person 2
+ *  returns their name and a
+ *  modified string 
+ *
+ * @author Qu
+ * @version 1.1
+ */
 public class Person2 {
    /** Holds the persons real name */
    private String name;
