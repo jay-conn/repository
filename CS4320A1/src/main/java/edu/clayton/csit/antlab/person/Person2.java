ef33628554f8b0c4744f251ea534295d764a2c63
david92569 <89665621+david92569@users.noreply.github.com>
2021-08-27 18:44:33 -0400

    Update Person2.java

--- CS4320A1/src/main/java/edu/clayton/csit/antlab/person/Person2.java
+++ CS4320A1/src/main/java/edu/clayton/csit/antlab/person/Person2.java
@@ -1,69 +1,55 @@
-package edu.clayton.csit.antlab.person;
+import java.util.Arrays;
+import java.util.List;
 
-/**
- *  A simple class for person 2
- *  returns their name and a
- *  modified string 
- *
- * @author Qu
- * @version 1.1
- */
 public class Person2 {
-    /** Holds the persons real name */
-    private String name;
-	 	/**
-	 * The constructor, takes in the persons
-	 * name
-	 * @param pname the person's real name
-	 */
-	 public Person2(String pname) {
-	   name = pname;
-	 }
+   /** Holds the persons real name */
+   private String name;
 
-/**
-	 * This method should take the string
-	 * input and return its characters in
-	 * random order.
-	 * given "gtg123b" it should return
-	 * something like "g3tb1g2".
-	 *
-	 * @param input the string to be modified
-	 * @return the modified string
-	 */
+   /**
+   * The constructor, takes in the persons name
+   *
+   * @param pname the person's real name
+   */
+   public Person2(String pname) {
+       name = pname;
+   }
 
-import java.util.Scanner;
-public class Person2 {
-/**this line is the main method to the program*/
-public static void main(String[] args) {
-//this line would create a new Scanner instance
-Scanner input = new Scanner(System.in);
+   /**
+   * This method should take the string input and return its characters in random
+   * order. given "gtg123b" it should return something like "g3tb1g2".
+   *
+   * @param input the string to be modified
+   * @return the modified string
+   */
+   private String calc(String input) {
+       // thus line will initialize new string
+       String newString = "";
+       // the next line would be converting string to list
+       List<String> characters = Arrays.asList(input.split(""));
+     
+       for (String value : characters) {
+           newString = newString + value;
+       }
+       // given this line we can return new string
+       return newString;
+   }
 
-//this line tell us to enter a string in order to print the line
-System.out.print("Enter string : ");
+   /**
+   * Return a string rep of this object that varies with an input string
+   *
+   * @param input the varying string
+   * @return the string representing the object
+   */
+   public String toString(String input) {
+       return name + calc(input);
+   }
 
+   public static void main(String[] args) {
+       // we are creating an object of Person2
+       Person2 person2 = new Person2("gtg123b");
+       //then we will print out the sting
+       System.out.println(person2.toString("gtg123b"));
 
-//this would advance the scanner past the current line and returns the input that was skipped.
-String str = input.nextLine();
-String result = calc(str);
-//process the line 
-System.out.println("Result is : " + result);
-//here we are printing out our result
-}
+   } end of method 
 
-
-/** here the method we used to get all numbers and store it*/
-private static String calc(String str) {
-if (str.length() > 2) {
-char first = str.charAt(0);
-char second = str.charAt(1);
-str = (str.substring(1)).substring(1);
-return str + first + second;
-}
-
-return str;
-
-} 
-
-} // end of method
-
-} // end of class
+} end of class
