7e3d40aa7ad8a5b0eee24da7660239ee95420917
jay-conn <62089432+jay-conn@users.noreply.github.com>
2021-08-27 18:50:56 -0400

    Update AntLabMain.java

--- CS4320A1/src/main/java/edu/clayton/csit/antlab/pkg1/AntLabMain.java
+++ CS4320A1/src/main/java/edu/clayton/csit/antlab/pkg1/AntLabMain.java
@@ -74,7 +74,7 @@ import edu.clayton.csit.antlab.pkg2.*;
                   //Person5 replace P4 with your name
                   //and csuLaker5 with your laker id
                   Person5 p5 = new Person5("P5");
-                  toPrint += p5.toString("csuLaker5");
+                  toPrint += p5.toString("Jahsi");
 		  
         System.out.println(toPrint);
         
